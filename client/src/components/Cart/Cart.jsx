d68e72adb53795aa0c4b033ba6274fcdfcc696d6
Yash Pimple <97302447+YashPimple@users.noreply.github.com>
2023-01-19 00:25:42 +0530

    Update Cart.jsx

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,52 +1,44 @@
 import React from 'react'
 import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
 import "./Cart.scss"
+import { useDispatch, useSelector } from 'react-redux';
+import { removeItem, resetCart } from '../../redux/cartReducer';
 const Cart = () => {
-  const data = [
-    {
-        id:1,
-        img: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600",
-        img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
-        title: "Long Sleeve Graphic T-shirt",
-        isNew: true,
-        oldPrice: 19,
-        price: 12,
-        desc: "Lorem24 uhcd nni dn  nn n j",
+  const products = useSelector((state) => state.cart.products);
+  const dispatch = useDispatch();
 
-    },
-    {
-        id:2,
-        img: "https://images.pexels.com/photos/1759622/pexels-photo-1759622.jpeg?auto=compress&cs=tinysrgb&w=1600",
-        img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
-        title: "Over-Sized T-shirt",
-        isNew: true,
-        oldPrice: 20,
-        price: 323,
-        desc: "Lorem24 uhcd nni dn  nn n j",
+  const totalPrice = () => {
+    let total = 0;
+    products.forEach((item) => {
+      total += item.quantity * item.price;
+    });
+    return total.toFixed(2);
+  };
 
-    },
-  ]
   return (
-    <div className='cart'>
-    <h1>Products in your Cart</h1>
-    {data.map(item=>{
-         return (
-                 <div className='item' key={item.id}>
-                  <img src={item.img} alt='' />
-                  <div className='details'>
-                    <h1>{item.title}</h1>
-                    <p>{item.desc?.substring(0,100)}</p>
-                    <div className='price'>1 X ${item.price}</div>
-                  </div>
-                  <DeleteOutlinedIcon className="delete"/>
+    <div className="cart">
+      <h1>Products in your cart</h1>
+      {products?.map((item) => (
+        <div className="item" key={item.id}>
+          <img src={process.env.REACT_APP_UPLOAD_URL + item.img} alt="" />
+          <div className="details">
+            <h1>{item.title}</h1>
+            <p>{item.desc?.substring(0, 100)}</p>
+            <div className="price">
+              {item.quantity} x ${item.price}
+            </div>
+          </div>
+                  <DeleteOutlinedIcon className="delete"
+                    onClick={() => dispatch(removeItem(item.id))}
+                  />
                  </div>)
-            })}
+            )}
             <div className="total">
                   <span>SUBTOTAL</span>
-                  <span>$123</span>
+                  <span>${totalPrice()}</span>
              </div>
               <button >PROCEED TO CHECKOUT</button>
-             <span className="reset" >
+             <span className="reset" onClick={() => dispatch(resetCart())}>
                 Reset Cart
              </span>
   
